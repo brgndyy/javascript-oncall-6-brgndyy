ec97d6f02f3bbdd9f85f742c9ce7f86abafb8d0f
brgndy <brgndy96@gmail.com>
2023-12-16 17:13:42 +0900

    Refactor: 해당 월 숫자만 받도록 유효성 검증 코드 추가

--- src/validators/MonthValidator.js
+++ src/validators/MonthValidator.js
@@ -5,6 +5,7 @@ import CONDITION from '../constants/conditions/condition.js';
 class MonthValidator {
   static validateMonth(month) {
     if (
+      !/^\d+$/.test(month) ||
       typeof Number(month) !== 'number' ||
       Number(month) < CONDITION.start_month ||
       Number(month) > CONDITION.end_month
