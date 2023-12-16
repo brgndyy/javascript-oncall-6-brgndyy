ef3573f51f20313efec764bce8fa89765dc0c386
brgndy <brgndy96@gmail.com>
2023-12-16 14:12:04 +0900

    Feat: 상수 객체 생성

---
+++ src/constants/messages/errorMessage.js
@@ -0,0 +1,7 @@
+import deepFreeze from '../../utils/deepFreeze.js';
+
+const ERROR_MESSAGES = deepFreeze({
+  invalid_month_and_day: '유효하지 않은 입력 값입니다. 다시 입력해 주세요.',
+});
+
+export default ERROR_MESSAGES;
