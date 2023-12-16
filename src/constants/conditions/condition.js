26284e1083f1204dd0d56bb3803da2021510dab9
brgndy <brgndy96@gmail.com>
2023-12-16 17:32:50 +0900

    Refactor: Condition 상수 객체 프로퍼티 추가

--- src/constants/conditions/condition.js
+++ src/constants/conditions/condition.js
@@ -2,6 +2,7 @@ import deepFreeze from '../../utils/deepFreeze.js';
 
 const CONDITION = deepFreeze({
   all_day: ['일', '월', '화', '수', '목', '금', '토'],
+  all_month_days: [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31],
   start_month: 1,
   end_month: 12,
   min_workers_length: 5,
