f6a31d1192901c9d560b4ca9686e2d44b3fa0165
brgndy <brgndy96@gmail.com>
2023-12-16 14:43:36 +0900

    Feat: 평일 비상 근무 입력 기능 구현

--- src/views/InputView.js
+++ src/views/InputView.js
@@ -6,7 +6,15 @@ const InputView = {
   async readMonthAndStartDay() {
     const monthAndStartDay = await Console.readLineAsync(PROGRESS_MESSAGE.read_month_and_start_day);
 
-    return monthAndStartDay.split(DELIMITER.comma);
+    return monthAndStartDay.split(DELIMITER.comma).map((str) => str.trim());
+  },
+
+  async readWeekDayEmergencyWorker() {
+    const weekDayEmergencyWorkers = await Console.readLineAsync(
+      PROGRESS_MESSAGE.read_week_day_emergency_worker,
+    );
+
+    return weekDayEmergencyWorkers.split(DELIMITER.comma).map((person) => person.trim());
   },
 };
 
