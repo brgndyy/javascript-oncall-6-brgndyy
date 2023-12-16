6c5225a1b4d3573a7adf4ab8390aa4577b2fcfe4
brgndy <brgndy96@gmail.com>
2023-12-16 17:27:27 +0900

    Feat: 각 월의 최대 일수를 담은 객체 생성

---
+++ src/database/allDate.js
@@ -0,0 +1,54 @@
+import deepFreeze from '../utils/deepFreeze.js';
+
+const ALL_DATE = deepFreeze([
+  {
+    month: 1,
+    maxDay: 31,
+  },
+  {
+    month: 2,
+    maxDay: 28,
+  },
+  {
+    month: 3,
+    maxDay: 31,
+  },
+  {
+    month: 4,
+    maxDay: 30,
+  },
+  {
+    month: 5,
+    maxDay: 31,
+  },
+  {
+    month: 6,
+    maxDay: 30,
+  },
+  {
+    month: 7,
+    maxDay: 31,
+  },
+  {
+    month: 8,
+    maxDay: 31,
+  },
+  {
+    month: 9,
+    maxDay: 30,
+  },
+  {
+    month: 10,
+    maxDay: 31,
+  },
+  {
+    month: 11,
+    maxDay: 30,
+  },
+  {
+    month: 12,
+    maxDay: 31,
+  },
+]);
+
+export default ALL_DATE;
