753dc9896f5a5066ac8873dc2afcc70d322b1e0e
brgndy <brgndy96@gmail.com>
2023-12-16 17:31:15 +0900

    Refactor: createCustomCalendar 함수 분리

--- src/service/createCustomCalendar.js
+++ src/service/createCustomCalendar.js
@@ -1,29 +1,29 @@
 import SPECIAL_HOLIDAY from '../database/holiday.js';
+import CONDITION from '../constants/conditions/condition.js';
+
+const isHoliday = (month, day, dayToNumber) => {
+  if (dayToNumber === 6 || dayToNumber === 0) {
+    return true;
+  }
+  return SPECIAL_HOLIDAY.some((holiday) => holiday.month === month && holiday.day === day);
+};
+
+const createDayEntry = (month, day, dayToNumber) => ({
+  month,
+  date: day,
+  dayToString: CONDITION.all_day[dayToNumber],
+  dayToNumber,
+  isHoliday: isHoliday(month, day, dayToNumber),
+});
 
 const createCustomCalendar = (month, startDayString) => {
   const monthDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-  const dayToString = ['일', '월', '화', '수', '목', '금', '토'];
   const daysInMonth = monthDays[month - 1];
-  let dayToNumber = dayToString.indexOf(startDayString);
+  let dayToNumber = CONDITION.all_day.indexOf(startDayString);
 
   const calendar = [];
   for (let day = 1; day <= daysInMonth; day++) {
-    let isHoliday = SPECIAL_HOLIDAY.some(
-      (holiday) => holiday.month === month && holiday.day === day,
-    );
-
-    if (dayToNumber === 6 || dayToNumber === 0) {
-      isHoliday = true;
-    }
-
-    calendar.push({
-      month,
-      date: day,
-      dayToString: dayToString[dayToNumber],
-      dayToNumber,
-      isHoliday,
-    });
-
+    calendar.push(createDayEntry(month, day, dayToNumber));
     dayToNumber = (dayToNumber + 1) % 7;
   }
 
